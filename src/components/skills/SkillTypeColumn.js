427c415a6c0c5863e7bb617c8b2a49e4694990b3
Erick Garcia <ericksilvagm@gmail.com>
2019-03-27 02:25:51 -0300

    integrates skills section in index page

--- src/components/skills/SkillTypeColumn.js
+++ src/components/skills/SkillTypeColumn.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
-
-
-
-
-
 export default function SkillTypeColumn({skillType, skillsInType}) {
     return (
         <article className="column">
-            <div className="message">
-                <div className="message-header">
-                    <p>{skillType}</p>
+            <div className="message is-medium">
+                <div className="message-header is-centered">
+                    <p className="has-text-info">{skillType}</p>
                 </div>
                 <div className="message-body is-three-quarters is-offset-2">
                     {skillsInType.map(({title, value}) => 
                         <div key={title}>
-                            {title}
-                            <progress className="progress is-info" value={value} max="100">{value}</progress>
+                            <span className="has-text-link is-size-5 has-text-weight-bold">{title}</span>
+                            <progress className="progress is-danger" value={value} max="100">{value}</progress>
                         </div>
                     )}
                 </div>
