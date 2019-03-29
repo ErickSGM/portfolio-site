8ffec1a8ab90453be366e654a68755ec32dd3f52
Erick Garcia <ericksilvagm@gmail.com>
2019-03-29 01:44:33 -0300

    added some style to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from 'react';
+
+import Image from '../components/Image';
+import Layout from '../components/Layout';
 
 const NotFoundPage = () => (
   <Layout>
     <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className="section">
+        <div className="container has-text-centered">
+          <p className="title is-1">404 NOT FOUND</p>
+          <p className="subtitle is-3">Sorry about that, maybe is something I'm currently working on</p>
+        </div>
+      </div>
+      <div className="section">
+        <div className="container has-text-centered">
+        <Image imageInfo={{image: '/img/404.jpg'}}/>
+        </div>
+      </div>
     </div>
   </Layout>
 )
